41f474a9cd90332cc8220804e36f05f1eb092a9c
Alan Torres <Alan.TorresGomez@bristol.ac.uk>
2014-10-22 20:31:38 +0100

    Changes on My-string-matching

--- src/string-matching/my-string-matching.cpp
+++ src/string-matching/my-string-matching.cpp
@@ -36,68 +36,60 @@ int main(int argc, char *argv[])
 
         cout << "\n\nNaive matching:\n===================================================\n\n";
         {
-            // 1) Start at possition 0 and increase by 1 on each iteration. Loop.
-            // Hint:
-            //     Get the length of a string with:   string_name.length()
-            for (unsigned int offset = 0; /* WRITE ME! */ ; offset++)
+            for (unsigned int i = 0; /* WRITE ME! */; i++) //[hint:str.length() for string length]
             {
                 print(str, pattern, offset);
 
-                // 2) For each character {j} on the pattern. Loop.
+                // 2) For each character the pattern. Loop.
                 for (/* WRITE ME! */)
                 {
-                    // 3) Compare character {i} to character {j}
-                    // Hint:
-                    //     Use: str.at(j + offset) == pattern.at(j)
-                    //     The offset comes from the starting position in step (1)
+                    // 3) Compare string to pattern [hint: str.at(j + offset) == pattern.at(j)]
                 }
 
-                // 4) Have you found the pattern?
-                // If you reached the end of the pattern and all characters matched, you found the solution
-                // If you found a character that doesn't match, you can skip to the next position (use 'break')
-
+                // 4) Have you found the end of the pattern?
                 if (/* WRITE ME! */)
-                    cout << "   IS A MATCH!" << endl << endl;
+                    cout << " IS A MATCH!" << endl << endl;
+
                 else
                     cout << endl << endl;
-
                 tools::wait(); // wait for user to press [enter]
             }
         }
 
         cout << "\n\nKnuth-Morris-Pratt:\n===================================================\n\n";
         {
-            KMPPrefixTable table(pattern);
+            KMPPrefixTable table(pattern); // Note: pre-fix table is automatically created
+
             unsigned int
                 n = str.length(),
                 m = pattern.length(),
                 i = 0;
-            
-            /*
-             *  Note: In c++, the indices start in 0.
-             *        The comments on this document are different from the slides to make this consideration
-             */
 
-            // for j = 0 upto n step 1 do   (Note j = 0)
+            /*
+            * Note: In c++, the indices start in 0.
+            * The comments on this document are thus different from the slides
+            */
+
+            // for j = 0 upto n step 1 do (Note j = 0)
             for (/* WRITE ME! */)
             {
-                // while i > 0 and P[i] != T[j] do.   (Note P[i])
+                // while i > 0 and P[i] != T[j] do.
                 while (/* WRITE ME! */)
                 {
-                    // Skip using prex table
+                    // Skip using prex table
                     i = table[i];
                 }
 
-                // if P[i] = T[j] then   (Note P[i])
+                // if P[i] = T[j] then (Note P[i])
                 if (/* WRITE ME! */)
                 {
                     // Next character matches
                 }
 
-                // if i = m then
+                // if you reach end of pattern [hint: i == m]
                 if (/* WRITE ME! */)
                 {
-                    // OUTPUT(j - m + 1)   (Note + 1)
+                    // OUTPUT(j - m + 1) (Note + 1)
                     // Look for next match
                 }
             }
@@ -106,52 +98,42 @@ int main(int argc, char *argv[])
         cout << "\n\nBoyer-Moore-Horspool:\n===================================================\n\n";
         {
             BMHTable table(pattern);
-            unsigned int
-                n = str.length(),             // Length of the string
-                m = pattern.length(),         // Length of the pattern
-                offset = 0,                   // Offset (position of the pattern)
-                i = m - 1,                    // Character to test in pattern
-                j = i + offset;               // Character to test in string
 
-            // 1) Repeat until we reach the end of the string
-            // 
-            // offset + m < n
+            unsigned int
+                n = str.length(), // Length of the string
+                m = pattern.length(), // Length of the pattern
+                offset = 0, // Offset (position of the pattern)
+                i = m - 1, // Character to test in pattern
+                j = i + offset; // Character to test in string
+
+            // 1) Repeat until we reach the end of the string [hint: offset + m < n]
             while (/* WRITE ME! */)
             {
                 // 2) Compare character {i} to character {j}
-                // Hint:
-                //     Use: str.at(j) == pattern.at(i)
                 if (/* WRITE ME! */)
                 {
                     // 3A) The character matches, test next one?
                     if (i < 0)
                     {
                         // We haven't reached the first character
-                        // modify {i} and {j} so we would test the next character (reverse order)
                     }
                     else
                     {
                         // We reached the first character, all matched, we found it!!
-                        // Print something here!
-
-
                         // exit
-                        break;
                     }
                 }
                 else
                 {
                     // 3B) The character didn't match, increase the offset
                     // Hint:
-                    //     Use the table to know how much to increment the offset:   unsigned char increment = table.at(str.at(j));
-
-                    // Reset indices
+                    // Use the table to know how much to increment the offset: unsigned char increment = table.at(str.at(j));
+                    // 4) Reset indices
                     i = m - 1;
                     j = i + offset;
                 }
             }
         }
-
     } // while
 
     cout << "End" << endl;
